0a8c1833c4cec513b8de719455232cb84f0973bf
nathan <nathan@janrain.com>
2011-03-31 15:20:01 -0700

    removed appId

--- SimpleDemo/src/com/janrain/android/simpledemo/MainActivity.java
+++ SimpleDemo/src/com/janrain/android/simpledemo/MainActivity.java
@@ -66,9 +66,8 @@ public class MainActivity extends Activity implements View.OnClickListener, JREn
 
     private static final int DIALOG_JRENGAGE_ERROR = 1;
 
-//    private static final String ENGAGE_APP_ID = "aehecdnjkodopeijgjgo"; //lilli's rpx dev environment RP
-    private static final String ENGAGE_APP_ID = "appcfamhnpkagijaeinl"; //rpxnow.com RP
-    private static final String ENGAGE_TOKEN_URL = null;//"http://jrengage-for-android.appspot.com/login";
+    //private static final String ENGAGE_APP_ID = "put your Engage appId here";
+    private static final String ENGAGE_TOKEN_URL = null;
 
     private JREngage mEngage;
     private Button mBtnTestAuth;
