8056061f93f5f20c3e1a7b86f73e988f8ec0b96b
Lilli <lilli@janrain.com>
2011-05-03 12:24:14 -0700

    Added BSD Copyrights and optimized imports

--- QuickShare/src/com/janrain/android/quickshare/Story.java
+++ QuickShare/src/com/janrain/android/quickshare/Story.java
@@ -1,3 +1,36 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ Copyright (c) 2011, Janrain, Inc.
+
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation and/or
+   other materials provided with the distribution.
+ * Neither the name of the Janrain, Inc. nor the names of its
+   contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ File:   Story.java
+ Author: Lilli Szafranski - lilli@janrain.com
+ Date:   April 22, 2011
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 package com.janrain.android.quickshare;
 
 import android.content.Context;
@@ -10,6 +43,7 @@ import android.os.AsyncTask;
 import android.text.Html;
 import android.util.Config;
 import android.util.Log;
+<<<<<<< Updated upstream
 import android.view.Gravity;
 import com.janrain.android.engage.session.JRSessionData;
 import org.w3c.dom.Document;
@@ -21,19 +55,17 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
+=======
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+>>>>>>> Stashed changes
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-
-/**
- * Created by IntelliJ IDEA.
- * User: lillialexis
- * Date: 4/22/11
- * Time: 1:12 PM
- * To change this template use File | Settings | File Templates.
- */
 public class Story implements Serializable {
     private static final String TAG = Story.class.getSimpleName();
 
