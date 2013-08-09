a64141ccd099cf49a389d5f5f05ca01a0a0320b3
Nathan <nathan@janrain.com>
2013-08-09 10:33:25 -0700

    updates release.sh to verify args

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]
+then
+  echo "Usage: `basename $0` vX.Y.Z"
+  exit 1
+fi
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source $SCRIPT_DIR/require_clean_work_tree.sh
